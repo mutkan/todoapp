f11c81322b74466fd47dc1ace952b037681ae99b
mutlu <mutkanx@gmail.com>
2019-06-22 15:09:02 -0500

    Add Task Activity and set main action

---
+++ app/src/main/java/com/mutlucelep/todoapp/tasks/TaskActivity.kt
@@ -0,0 +1,13 @@
+package com.mutlucelep.todoapp.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mutlucelep.todoapp.R
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+}

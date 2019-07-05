5565176553c90c9a3873dc297ac6e91caf94c0eb
mutlu <mutkanx@gmail.com>
2019-07-04 23:23:11 -0500

    Add entity annotation with table name to task model

--- app/src/main/java/com/mutlucelep/todoapp/data/Task.kt
+++ app/src/main/java/com/mutlucelep/todoapp/data/Task.kt
@@ -1,3 +1,10 @@
 package com.mutlucelep.todoapp.data
 
-data class Task(val title: String, val description: String, val id: String, val isCompleted: Boolean)
+import androidx.room.Entity
+
+
+@Entity(tableName = "tasks")
+data class Task(val title: String, val description: String, val id: String, val isCompleted: Boolean) {
+    val isActive: Boolean
+        get() = !isCompleted
+}

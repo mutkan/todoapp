2d74f274c44ad8737912e3dc115ac80797e2a81f
mutlu <mutkanx@gmail.com>
2019-07-22 23:46:20 -0500

    Add global executor pools for the whole application.

---
+++ app/src/main/java/com/mutlucelep/todoapp/utils/AppExecutors.kt
@@ -0,0 +1,26 @@
+package com.mutlucelep.todoapp.utils
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutors(
+    val diskIO: Executor = DiskIOThreadExecutor(),
+    val mainThreadHandler: Executor = MainThreadExecutor()
+) {
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable?) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+    private class DiskIOThreadExecutor : Executor {
+        private val diskIO = Executors.newSingleThreadExecutor()
+
+        override fun execute(command: Runnable?) {
+            diskIO.execute(command)
+        }
+    }
+}

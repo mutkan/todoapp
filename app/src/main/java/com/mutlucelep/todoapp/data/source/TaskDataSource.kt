785fdbba14d2c00c682b9d1db826e99e8e9a4757
mutlu <mutkanx@gmail.com>
2019-07-22 23:52:36 -0500

    Add some interfaces

--- app/src/main/java/com/mutlucelep/todoapp/data/source/TaskDataSource.kt
+++ app/src/main/java/com/mutlucelep/todoapp/data/source/TaskDataSource.kt
@@ -3,18 +3,24 @@ package com.mutlucelep.todoapp.data.source
 import com.mutlucelep.todoapp.data.Task
 
 interface TaskDataSource {
-    interface LoadTasksCallback{
+    interface LoadTasksCallback {
         fun onTasksLoaded(tasks: List<Task>)
         fun onDataNotAvailable()
     }
 
-    interface GetTaskCallback{
+    interface GetTaskCallback {
         fun onTaskLoaded(task: Task)
         fun onDataNotAvailable()
     }
 
     fun getTasks(callback: LoadTasksCallback)
+    fun getTask(taskId: String, callback: GetTaskCallback)
     fun completeTask(task: Task)
+    fun completeTask(taskId: String)
     fun activateTask(task: Task)
+    fun activateTask(taskId: String)
     fun deleteAllTask()
+    fun deleteTask(taskId: String)
+    fun saveTask(task: Task)
+    fun clearCompletedTasks()
 }

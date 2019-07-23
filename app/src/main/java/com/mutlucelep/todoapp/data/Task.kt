5e1ac5f78d79473badf7ad1c7b58e2a2565aab67
mutlu <mutkanx@gmail.com>
2019-07-22 23:40:48 -0500

    Remove isCompleted property from constructor, because when you use apply scope func for changing a property in constructor, it does not work

--- app/src/main/java/com/mutlucelep/todoapp/data/Task.kt
+++ app/src/main/java/com/mutlucelep/todoapp/data/Task.kt
@@ -1,10 +1,15 @@
 package com.mutlucelep.todoapp.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
 
 
 @Entity(tableName = "tasks")
-data class Task(val title: String, val description: String, val id: String, val isCompleted: Boolean) {
+data class Task(val title: String, val description: String, @PrimaryKey val id: String = UUID.randomUUID().toString()) {
+
+    var isCompleted : Boolean = false
+
     val isActive: Boolean
         get() = !isCompleted
 }

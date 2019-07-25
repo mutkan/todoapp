3a4fb9c98e6cfc290cc85d86b6bffba26d881cd6
mutlu <mutkanx@gmail.com>
2019-07-25 13:39:29 -0500

    Add task local data source test file

---
+++ app/src/test/java/com/mutlucelep/todoapp/data/source/local/TaskLocalDataSourceTest.kt
@@ -0,0 +1,20 @@
+package com.mutlucelep.todoapp.data.source.local
+
+import org.junit.Before
+
+class TaskLocalDataSourceTest {
+    private val TASK_TITLE_1 = "TITLE1"
+    private val TASK_TITLE_2 = "TITLE2"
+    private val TASK_TITLE_3 = "TITLE3"
+    private val TASK_DESCRIPTION_1 = "DESCRIPTION1"
+    private val TASK_DESCRIPTION_2 = "DESCRIPTION2"
+
+    private lateinit var taskLocalDataSource: TaskLocalDataSource
+
+
+    @Before
+    fun setUp() {
+
+    }
+}
+

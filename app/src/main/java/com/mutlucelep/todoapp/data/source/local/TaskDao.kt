ae46f8b2f4e4910181d424527a4a47183c46e2d9
mutlu <mutkanx@gmail.com>
2019-07-25 17:58:19 -0500

    Add insert and get task by id func to task dao interface

--- app/src/main/java/com/mutlucelep/todoapp/data/source/local/TaskDao.kt
+++ app/src/main/java/com/mutlucelep/todoapp/data/source/local/TaskDao.kt
@@ -1,10 +1,19 @@
 package com.mutlucelep.todoapp.data.source.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mutlucelep.todoapp.data.Task
 
 @Dao
-interface TaskDao{
-    @Query("SELECT * FROM tasks") fun getTasks(): List<Task>
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getTasks(): List<Task>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(task: Task)
+
+    @Query("SELECT * FROM tasks WHERE id=:taskId")
+    fun getTaskById(taskId: String): Task?
 }

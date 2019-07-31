d2f73d85df5e4f988cbdca894b9438b31cc94ab1
mutlu <mutkanx@gmail.com>
2019-07-30 23:52:28 -0500

    Revision

--- app/src/test/java/com/mutlucelep/todoapp/data/source/utils/SingleExecutors.kt
+++ app/src/test/java/com/mutlucelep/todoapp/data/source/utils/SingleExecutors.kt
@@ -1,8 +1,9 @@
 package com.mutlucelep.todoapp.data.source.utils
 
+import com.mutlucelep.todoapp.utils.AppExecutors
 import java.util.concurrent.Executor
 
-class SingleExecutors : AppExecutors(instant,instant) {
+class SingleExecutors : AppExecutors(instant, instant) {
     companion object {
         private val instant = Executor { command -> command.run() }
     }

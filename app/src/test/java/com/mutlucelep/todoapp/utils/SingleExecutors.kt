7e70c97d27da8ceae10ba86cb698f53937f199cb
mutlu <mutkanx@gmail.com>
2019-07-30 23:52:16 -0500

    Remove single executors from androidTest folder

--- app/src/test/java/com/mutlucelep/todoapp/utils/SingleExecutors.kt
+++
@@ -1,9 +0,0 @@
-package com.mutlucelep.todoapp.utils
-
-import java.util.concurrent.Executor
-
-class SingleExecutors : AppExecutors(instant) {
-    companion object {
-        private val instant = Executor { command -> command.run() }
-    }
-}

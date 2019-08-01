02ee4bb7722a8e6e0ff809ef3ce1296b0266dae4
mutlu <mutkanx@gmail.com>
2019-08-01 14:18:36 -0500

    Add test to check when you push back button at home screen, it should exit

--- app/src/androidTest/java/com/mutlucelep/todoapp/tasks/AppNavigationTest.kt
+++ app/src/androidTest/java/com/mutlucelep/todoapp/tasks/AppNavigationTest.kt
@@ -1,9 +1,26 @@
 package com.mutlucelep.todoapp.tasks
 
+import android.view.Gravity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.NoActivityResumedException
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.DrawerMatchers.isOpen
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import com.mutlucelep.todoapp.R
+import com.mutlucelep.todoapp.utils.TestUtils.getToolbarNavigationContentDescription
+import junit.framework.Assert
+import org.junit.Assert.fail
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -31,4 +48,18 @@ class AppNavigationTest {
             .check(matches(isOpen(Gravity.START)))
     }
 
+    @Test
+    fun backFromTasksScreen_ExitsApp() {
+        assertPressingBackExitsApp()
+    }
+
+    private fun assertPressingBackExitsApp() {
+        try {
+            pressBack()
+            fail("Should kill the app and throw an exception")
+        } catch (e: NoActivityResumedException) {
+            // Test OK
+        }
+
+    }
 }

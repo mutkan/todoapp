04681d2a8aee2c917f1bdb786eb2af3584a996e7
mutlu <mutkanx@gmail.com>
2019-08-02 14:24:55 -0500

    Add dependencies package name

--- app/src/test/java/com/mutlucelep/todoapp/addedittask/AddEditTaskPresenterTest.kt
+++ app/src/test/java/com/mutlucelep/todoapp/addedittask/AddEditTaskPresenterTest.kt
@@ -1,8 +1,20 @@
 package com.mutlucelep.todoapp.addedittask
 
+import com.mutlucelep.todoapp.any
+import com.mutlucelep.todoapp.capture
+import com.mutlucelep.todoapp.data.Task
+import com.mutlucelep.todoapp.data.source.TaskDataSource
+import com.mutlucelep.todoapp.data.source.TaskRepository
+import com.mutlucelep.todoapp.eq
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
 class AddEditTaskPresenterTest {

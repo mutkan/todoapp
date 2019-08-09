7102b6b54c8f6e0c6339226c4839957795b5172c
mutlu <mutkanx@gmail.com>
2019-08-08 22:32:27 -0500

    Add test for task local datasource

--- app/src/test/java/com/mutlucelep/todoapp/data/source/local/TaskLocalDataSourceTest.kt
+++ app/src/test/java/com/mutlucelep/todoapp/data/source/local/TaskLocalDataSourceTest.kt
@@ -1,20 +1,38 @@
 package com.mutlucelep.todoapp.data.source.local
 
+import androidx.room.Room
+import com.mutlucelep.todoapp.data.Task
+import com.mutlucelep.todoapp.data.source.TaskDataSource
+import com.mutlucelep.todoapp.data.source.utils.SingleExecutors
+import org.hamcrest.core.Is.`is`
+import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import java.util.*
 
+@RunWith(RobolectricTestRunner::class)
 class TaskLocalDataSourceTest {
-    private val TASK_TITLE_1 = "TITLE1"
-    private val TASK_TITLE_2 = "TITLE2"
-    private val TASK_TITLE_3 = "TITLE3"
-    private val TASK_DESCRIPTION_1 = "DESCRIPTION1"
-    private val TASK_DESCRIPTION_2 = "DESCRIPTION2"
-
     private lateinit var taskLocalDataSource: TaskLocalDataSource
+    private lateinit var database: ToDoDatabase
 
 
     @Before
     fun setUp() {
+        database = Room.inMemoryDatabaseBuilder(RuntimeEnvironment.systemContext, ToDoDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
 
+        TaskLocalDataSource.clearInstance()
+        taskLocalDataSource = TaskLocalDataSource.getInstance(SingleExecutors(), database.taskDao())
     }
-}
 
+    @After
+    fun endUp() {
+        database.close()
+        TaskLocalDataSource.clearInstance()
+    }
